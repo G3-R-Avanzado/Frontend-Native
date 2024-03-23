e12044552c37aaa0f5f658924f400bf560a64de8
Mauricio Soria <mauricioutn2017@gmail.com>
2024-03-23 17:43:07 -0300

    feature: Mensaje de bienvenida

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useEffect, useState } from 'react';
+import {Image, StyleSheet, Text, View } from 'react-native';
+import logo from "./assets/logo1.png"
 export default function App() {
+  const [bienvenido, setBienvenida] = useState(true)
+  const bienvenida=()=>{
+    setBienvenida(false)
+  }
+  useEffect(()=>{
+    setTimeout(() => {
+      bienvenida();
+    }, 2000);
+  },[])
+  if(bienvenido){
+    return(
+      <View style={styles.bienvenida}>
+        <Image source={logo}/>
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -17,4 +34,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bienvenida:{
+    flex:1,
+    backgroundColor:"#FFE600",
+    alignItems: "center",
+    justifyContent:"center"
+  }
 });
