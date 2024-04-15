7e3c97fa616db03f5d8fe041be34aa0568239290
Mauricio Soria <mauricioutn2017@gmail.com>
2024-04-15 18:14:09 -0300

    feature: hooks

--- src/types/types.js
+++ src/types/types.js
@@ -4,3 +4,17 @@ export const Roles = {
     User: "Customer" 
 }
 
+export const mensajesToast={
+    inicioDeSesion:{
+        mensaje:"Inicio de sesión exitoso! Disfrutá comprando",
+        titulo:"Iniciaste sesión",
+        tipo:"toast",
+        icono:"success"
+    },
+    falloDeInicio:{
+        mensaje:"Inicio de sesión fallido!",
+        titulo:"Verificá los datos e intenta nuevamente",
+        tipo:"toast",
+        icono:"fail"
+    }
+}
